60b7192d0637697cd5402fb2694b605428103401
Nehal <ncsubscriberacc@gmail.com>
2019-08-25 23:16:23 +0530

    final update

---
+++ build/precache-manifest.34dfb339c7e4e903a7189a3c88c6cc84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37a07a972b483f33104c686c2435029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba084dde6e745490eb7",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "541af58e65e38baab592",
+    "url": "/static/js/2.0eba818f.chunk.js"
+  },
+  {
+    "revision": "eba084dde6e745490eb7",
+    "url": "/static/js/main.2c5901c4.chunk.js"
+  },
+  {
+    "revision": "797403e7520a83654a8f",
+    "url": "/static/js/runtime~main.cd73cb1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
